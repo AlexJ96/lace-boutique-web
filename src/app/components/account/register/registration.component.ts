369e8b145b499ae7caabd84305246d4d82d014c2
NgJason <Jason@ngjasons-mbp.mynet>
2018-12-19 22:55:41 +0000

    Added form validation to Regitration page. Will validate inputs from both web server and the api server.

--- src/app/components/account/register/registration.component.ts
+++ src/app/components/account/register/registration.component.ts
@@ -1,5 +1,6 @@
 import { OnInit, Component, ReflectiveInjector } from "@angular/core";
-import { NgForm } from '@angular/forms';
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+
 
 import { ApiService } from "../../../services/api.service";
 
@@ -10,14 +11,31 @@ import { ApiService } from "../../../services/api.service";
     styleUrls: ['./registration.component.css']
 })
 
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+
+    public registerServerErrorMessage : String;
 
     constructor(private api: ApiService) {}
+
+    async ngOnInit() {
+        this.registerServerErrorMessage = '';
+        let token = await  this.api.get("token/request-token");
+        
+        console.log(token);
+    }
 
     async register(f){
         let data = f.value;
         let res = await this.api.post("account/register", data);
-        console.log(res);
+        if (res !== 'Done'){
+            this.registerServerErrorMessage = res;
+        }
+    	
     }
+
+    get registerServerErrorMsg(){
+        return this.registerServerErrorMessage;
+    }
+
 
 }
